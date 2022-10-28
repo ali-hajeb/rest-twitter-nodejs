304fe3f4ab752f9eac50355713fe7f65efc3d03d
ali-hajeb <ali.hajeb@hotmail.com>
2022-10-28 21:52:24 +0330

    Changed MongoDB database connection link

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function initMongo() {
-	mongoose.connect("mongodb://localhost:27017/test", {
+	mongoose.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	});
